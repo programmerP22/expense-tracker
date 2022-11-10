342e46b6efcffed55e51bda6c3c742b47f3e5443
programmerP22 <chiangcw19@gmail.com>
2022-11-10 16:56:43 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('hello')
